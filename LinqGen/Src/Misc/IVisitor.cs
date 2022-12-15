53bfe0d830594eca53838db1dc099d85b355a325
Maxwell Keonwoo Kang <code.athei@gmail.com>
2022-12-14 20:30:49 -0800

    organize directory

---
+++ LinqGen/Src/Misc/IVisitor.cs
@@ -0,0 +1,15 @@
+﻿// LinqGen, Maxwell Keonwoo Kang <code.athei@gmail.com>, 2022
+
+using System;
+using System.Collections;
+
+namespace Cathei.LinqGen.Hidden
+{
+    /// <summary>
+    /// Visitor that local evaluations will implement
+    /// </summary>
+    public interface IVisitor<in T> : IDisposable
+    {
+        bool Visit(T element);
+    }
+}
