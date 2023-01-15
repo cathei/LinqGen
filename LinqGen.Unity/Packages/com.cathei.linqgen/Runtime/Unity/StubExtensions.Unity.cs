d8298d65b2c61e712cda77c8d09f31231ca8d127
Lukas Reuter <lukasreuter@outlook.com>
2023-01-15 20:04:36 +0100

    Added the common Unity collection types NativeList<T> and FixedListXBytes<T> to the StubExtensions
    so that they can be used by the source generator.

--- LinqGen.Unity/Packages/com.cathei.linqgen/Runtime/Unity/StubExtensions.Unity.cs
+++ LinqGen.Unity/Packages/com.cathei.linqgen/Runtime/Unity/StubExtensions.Unity.cs
@@ -14,5 +14,41 @@ namespace Cathei.LinqGen
         {
             throw new NotImplementedException();
         }
+
+        public static Stub<IEnumerable<T>, Gen<NativeList<T>>> Gen<T>(this NativeList<T> enumerable)
+            where T : unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Stub<IEnumerable<T>, Gen<FixedList32Bytes<T>>> Gen<T>(this FixedList32Bytes<T> enumerable)
+            where T : unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Stub<IEnumerable<T>, Gen<FixedList64Bytes<T>>> Gen<T>(this FixedList64Bytes<T> enumerable)
+            where T : unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Stub<IEnumerable<T>, Gen<FixedList128Bytes<T>>> Gen<T>(this FixedList128Bytes<T> enumerable)
+            where T : unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Stub<IEnumerable<T>, Gen<FixedList512Bytes<T>>> Gen<T>(this FixedList512Bytes<T> enumerable)
+            where T : unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Stub<IEnumerable<T>, Gen<FixedList4096Bytes<T>>> Gen<T>(this FixedList4096Bytes<T> enumerable)
+            where T : unmanaged
+        {
+            throw new NotImplementedException();
+        }
     }
 }
