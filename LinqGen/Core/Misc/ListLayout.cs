c1dd806e5c7071183a4dead5a7ef390b5d9d0296
Maxwell Kang <code.athei@gmail.com>
2023-10-12 02:34:19 -0700

    fix unity ListLayout

--- LinqGen/Core/Misc/ListLayout.cs
+++ LinqGen/Core/Misc/ListLayout.cs
@@ -4,10 +4,11 @@ using System;
 
 namespace Cathei.LinqGen.Hidden
 {
+    // Unity List<T> layout: https://github.com/Unity-Technologies/UnityCsReference/blob/3d74e124da422b5ab79e573ca4d5b4829ba09c34/Runtime/Export/Scripting/NoAllocHelpers.bindings.cs#L78
     public class ListLayout<T>
     {
         public T[] Items;
-#if !NETCOREAPP3_0_OR_GREATER
+#if !UNITY_5_3_OR_NEWER && !NETCOREAPP3_0_OR_GREATER
         public Object SyncRoot;
 #endif
         public int Size;
